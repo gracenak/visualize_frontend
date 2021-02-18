70a27e44b2c33c04c0477c1777b6b4bf692f9f8f
gracenak <gracenak@gmail.com>
2021-02-18 11:31:28 -0700

    refactor render of the vision object

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const visionsURL = "http://localhost:3000/api/v1/visions"
+
 document.addEventListener('DOMContentLoaded', () => {
     alert('LOADED');
     getVisions()
@@ -15,19 +16,25 @@ function getVisions() {
              // remember our JSON data is a bit nested due to our serializer
       visions.data.forEach(vision => {
         // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-        const visionMarkup = `
+        renderVision(vision)
+      })
+    //   .catch(err => console.log(err))
+    })
+}
+
+function renderVision(vision) {
+    const visionMarkup = `
           <div data-id=${vision.id}>
             <img src=${vision.attributes.image_url} height="200" width="250">
             <h3>${vision.attributes.title}</h3>
             <h4>${vision.attributes.description}</h4>
-            <p>${vision.attributes.user.name}</p>
+            <p>${vision.attributes.theme.name}</p>
             <button data-id=${vision.id}>Edit</button>
+            <button data-id=${vision.id} class='delete-btn'>Delete</button>
           </div>
           <br><br>`;
 
           document.querySelector('#vision-container').innerHTML += visionMarkup
-      })
-    })
 }
 
 function createFormHandler(e) {
@@ -35,13 +42,13 @@ function createFormHandler(e) {
     const titleInput = document.querySelector("#input-title").value
     const descriptionInput = document.querySelector("#input-description").value
     const imgInput = document.querySelector("#input-url").value
-    const userId = parseInt(document.querySelector("#users").value)
-    postFetch(titleInput, descriptionInput, imgInput, userId)
+    const themeId = parseInt(document.querySelector("#themes").value)
+    postFetch(titleInput, descriptionInput, imgInput, themeId)
 
 } 
 
-function postFetch(title, description, image_url, user_id) {
-    const bodyData = {title, description, image_url, user_id}
+function postFetch(title, description, image_url, theme_id) {
+    const bodyData = {title, description, image_url, theme_id}
     fetch(visionsURL, {
     method: "POST",
     headers: {
@@ -52,19 +59,25 @@ function postFetch(title, description, image_url, user_id) {
     })
     .then(response => response.json())
     .then(vision => {
-        const visionData = vision.data.attributes
-        const visionMarkup = `
-        <div data-id=${vision.id}>
-            <img src=${visionData.image_url} height="200" width="250">
-            <h3>${visionData.title}</h3>
-            <h4>${visionData.description}</h4>
-            <p>${visionData.user.name}</p>
-            <button data-id=${visionData.id}>Edit</button>
-        </div>
-        <br><br>`;
-  
-      document.querySelector('#vision-container').innerHTML += visionMarkup;
+        const visionData = vision.data
+        renderVision(visionData)
     })
+  }
+
+  function solve() {
+      console.log(e)
+      let visionCollection = document.querySelector('#vision-container')
+      visionCollection.addEventListener('click', function(e) {
+          e.preventDefault()
+          deleteVision(e)
+          e.target.parentElement.remove()
+      })
+  }
+
+  function deleteVision(e) {
+      fetch(`${visionsURL}/${e.target.dataset.id}`, {
+          method: "DELETE"
+      })
   }
 
 
